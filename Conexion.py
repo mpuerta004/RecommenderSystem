73c67cffdef3c1eae8b364319a3dee0d576fba95
mpuerta004 <mpuerta004@hotmail.com>
2022-09-30 16:11:31 +0200

    Establecer conexión con la base de datos en mysql y meter los datos.
    No se si se va a realizar con mysql pero bueno puedo ir probando y haciendo cosas, y refinando lo que seria el primer modelo de obtener la prioridad de las celdas.

---
+++ Conexion.py
@@ -0,0 +1,55 @@
+import mysql.connector
+import Conf
+
+class Connexion:
+
+    def __init__(self):
+        self.client = None
+        self.cursor=None
+
+    def star(self):
+        if self.client == None:
+            # cur.execute( "SELECT name FROM User" )
+            try:
+                self.client = mysql.connector.connect(host='localhost', user='root', passwd='Sesamo940', db='SocioBee')
+                self.cursor=self.client.cursor()
+
+                return True
+            except:
+                print("No se ha podido establecer la conexión:")
+                return False
+        else:
+            return True
+
+    def close(self):
+        if self.client is None:
+            return True
+        else:
+            try:
+                self.client.close()
+                self.client = None
+                self.cursor= False
+                return True
+            except:
+                print("No se ha podido cerrar la conexión")
+                return False
+
+
+
+
+if __name__ == '__main__':
+    bd = Connexion()
+    print(bd.star())
+
+
+    bd.cursor.execute("INSERT into QueenBee (name, surname, age) values ('tralala','tralalala2',40);")#;'
+    bd.client.commit()
+    print(bd.cursor.fetchall)
+    bd.cursor.execute("Select* from QueenBee")
+    for i in bd.cursor.fetchall():
+        print(i)
+
+
+    #                  'INSERT  INTO Campaign (createdBy, city) values (1,"Zaragoza")',multi=True )
+    #a=bd.cursor.execute("Select* from QueenBee")
+    bd.close()
