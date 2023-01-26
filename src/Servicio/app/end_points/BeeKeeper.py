03f6276f644af0fae51068872d12199898e07c0e
mpuerta004 <mpuerta004@hotmail.com>
2023-01-26 12:28:34 +0100

    Update BeeKeeper.py

--- src/Servicio/app/end_points/BeeKeeper.py
+++ src/Servicio/app/end_points/BeeKeeper.py
@@ -7,11 +7,11 @@ from pathlib import Path
 from sqlalchemy.orm import Session
 from schemas.Measurement import Measurement, MeasurementCreate, MeasurementSearchResults
 from schemas.Campaign import CampaignSearchResults, Campaign, CampaignCreate
-from schemas.Slot import Slot, SlotCreate,SlotSearchResults
+from schemas.Slot import Slot, SlotCreate, SlotSearchResults
 from schemas.Hive import Hive, HiveCreate, HiveSearchResults
-from schemas.BeeKeeper import BeeKeeper,BeeKeeperCreate,BeeKeeperSearchResults, BeeKeeperUpdate
+from schemas.BeeKeeper import BeeKeeper, BeeKeeperCreate, BeeKeeperSearchResults, BeeKeeperUpdate
 
-from schemas.CampaignRole import CampaignRole,CampaignRoleCreate,CampaignRoleSearchResults, CampaignRoleUpdate
+from schemas.CampaignRole import CampaignRole, CampaignRoleCreate, CampaignRoleSearchResults, CampaignRoleUpdate
 from schemas.Priority import Priority, PriorityCreate, PrioritySearchResults
 from datetime import datetime, timedelta
 from schemas.Cell import Cell, CellCreate, CellSearchResults, Point
@@ -30,38 +30,35 @@ from enum import Enum, IntEnum
 api_router_beekeepers = APIRouter(prefix="/beekeepers")
 
 
-
-
 @api_router_beekeepers.get("/{beekeeper_id}", status_code=200, response_model=BeeKeeper)
 def get_a_beekeeper(
     *,
-    Beekeeper_id:int,
+    Beekeeper_id: int,
     db: Session = Depends(deps.get_db),
 ) -> Cell:
     """
     Get a BeeKeeper
     """
-    
-    beekeeper=crud.beekeeper.get_by_id(db=db, id=Beekeeper_id)
 
-    if  beekeeper is None:
+    beekeeper = crud.beekeeper.get_by_id(db=db, id=Beekeeper_id)
+
+    if beekeeper is None:
         raise HTTPException(
             status_code=404, detail=f"BeeKeeper with BeeKeeper_id=={Beekeeper_id} not found"
         )
     return beekeeper
 
 
-
 @api_router_beekeepers.delete("/{beekeeper_id}", status_code=204)
-def delete_beekeeper(    *,
-    beekeeper_id:int,
-    db: Session = Depends(deps.get_db),
-):
+def delete_beekeeper(*,
+                     beekeeper_id: int,
+                     db: Session = Depends(deps.get_db),
+                     ):
     """
     Delete a BeeKeeper.
     """
-    beekeeper=crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
-    if  beekeeper is None:
+    beekeeper = crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
+    if beekeeper is None:
         raise HTTPException(
             status_code=404, detail=f"BeeKeeper with id=={beekeeper_id} not found"
         )
@@ -73,43 +70,46 @@ def delete_beekeeper(    *,
         )
     return {"ok": True}
 
-@api_router_beekeepers.post("/",status_code=201, response_model=BeeKeeper )
+
+@api_router_beekeepers.post("/", status_code=201, response_model=BeeKeeper)
 def create_beekeeper(
-    *,    
+    *,
     recipe_in: BeeKeeperCreate,
     db: Session = Depends(deps.get_db)
 ) -> dict:
     """
     Create a new BeeKeeper of the hive in the database.
     """
-    BeeKeeper=BeeKeeperCreate(name=recipe_in.name,surname=recipe_in.surname,age=recipe_in.age,city=recipe_in.city,mail=recipe_in.mail,gender=recipe_in.gender,real_user=recipe_in.real_user)
-    try: 
-        BeeKeeper_new= crud.beekeeper.create(db=db, obj_in=BeeKeeper)
+    BeeKeeper = BeeKeeperCreate(name=recipe_in.name, surname=recipe_in.surname, age=recipe_in.age,
+                                city=recipe_in.city, mail=recipe_in.mail, gender=recipe_in.gender, real_user=recipe_in.real_user)
+    try:
+        BeeKeeper_new = crud.beekeeper.create(db=db, obj_in=BeeKeeper)
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error creating the Beekeeper: {e}"
         )
     return BeeKeeper_new
-   
+
 
 @api_router_beekeepers.put("/{beekeeper_id}", status_code=201, response_model=BeeKeeper)
 def put_a_beekeeper(
     *,
-    beekeeper_id:int,
+    beekeeper_id: int,
     recipe_in: BeeKeeperUpdate,
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
     Update a BeeKeeper
     """
-    beekeeper=crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
+    beekeeper = crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
 
-    if  beekeeper is None:
+    if beekeeper is None:
         raise HTTPException(
             status_code=404, detail=f"BeeKeeper with BeeKeeper_id=={beekeeper_id} not found"
         )
     try:
-        updated_beekeeper = crud.beekeeper.update(db=db, db_obj=beekeeper, obj_in=recipe_in)
+        updated_beekeeper = crud.beekeeper.update(
+            db=db, db_obj=beekeeper, obj_in=recipe_in)
         db.commit()
     except Exception as e:
         raise HTTPException(
@@ -118,25 +118,25 @@ def put_a_beekeeper(
     return beekeeper
 
 
-
 @api_router_beekeepers.patch("/{beekeeper_id}", status_code=201, response_model=BeeKeeper)
 def patch_a_beekeeper(
     *,
-    beekeeper_id:int,
+    beekeeper_id: int,
     recipe_in: Union[BeeKeeperUpdate, Dict[str, Any]],
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
     Update a BeeKeeper
     """
-    beekeeper=crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
+    beekeeper = crud.beekeeper.get_by_id(db=db, id=beekeeper_id)
 
-    if  beekeeper is None:
+    if beekeeper is None:
         raise HTTPException(
             status_code=404, detail=f"BeeKeeper with BeeKeeper_id=={beekeeper_id} not found"
         )
     try:
-        updated_beekeeper = crud.beekeeper.update(db=db, db_obj=beekeeper, obj_in=recipe_in)
+        updated_beekeeper = crud.beekeeper.update(
+            db=db, db_obj=beekeeper, obj_in=recipe_in)
         db.commit()
     except Exception as e:
         raise HTTPException(
