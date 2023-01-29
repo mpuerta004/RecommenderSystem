4e0f4eb965c9a57875f95953a2dba0ee2f16b001
mpuerta004 <mpuerta004@hotmail.com>
2023-01-29 15:23:36 +0000

    cell endpoint update

--- src/Servicio/app/end_points/Cells.py
+++ src/Servicio/app/end_points/Cells.py
@@ -32,6 +32,10 @@ from fastapi import BackgroundTasks
 # from end_points.Campaigns import create_slots
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
 
+import asyncio
+from fastapi_utils.session import FastAPISessionMaker
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:mypasswd@localhost:3306/SocioBee"
+sessionmaker = FastAPISessionMaker(SQLALCHEMY_DATABASE_URL)
 
 api_router_cell = APIRouter(prefix="/surfaces/{surface_id}/cells")
 
@@ -45,12 +49,21 @@ def search_all_cells_of_surface(
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
-    Search all cells of the surface_id of the campaign_id of the hive_id
+    Search all cells of a surface of a campaign of a hive
     """
+    hive = crud.hive.get(db=db, id=hive_id)
+    if hive is None:
+        raise HTTPException(status_code=404, detail=f"Hive with id=={hive_id} not found"  )
+    
+    Campaign = crud.campaign.get_campaign(db=db, campaign_id=campaign_id, hive_id=hive_id)
+    if Campaign is None:
+        raise HTTPException(
+            status_code=404, detail=f"Campaign with id=={campaign_id} not found"
+        )
     surface = crud.surface.get_surface_by_ids(db=db, surface_id=surface_id,campaign_id=campaign_id)
     if  surface is None:
         raise HTTPException(
-            status_code=404, detail=f"Surface with IDs  campaign_id {campaign_id} and surface_id {surface_id} not found"
+            status_code=404, detail=f"Surface with IDs id=={surface_id}  not found"
         )
     return {"results": list(surface.cells)}
 
@@ -66,10 +79,24 @@ def get_cell(
     """
     Get a cell
     """
+    hive = crud.hive.get(db=db, id=hive_id)
+    if hive is None:
+        raise HTTPException(status_code=404, detail=f"Hive with id=={hive_id} not found"  )
+    
+    Campaign = crud.campaign.get_campaign(db=db, campaign_id=campaign_id, hive_id=hive_id)
+    if Campaign is None:
+        raise HTTPException(
+            status_code=404, detail=f"Campaign with id=={campaign_id} not found"
+        )
+    surface = crud.surface.get_surface_by_ids(db=db, surface_id=surface_id,campaign_id=campaign_id)
+    if  surface is None:
+        raise HTTPException(
+            status_code=404, detail=f"Surface with IDs id=={surface_id}  not found"
+        )
     result = crud.cell.get_Cell(db=db, cell_id=cell_id, surface_id=surface_id, campaign_id=campaign_id)
     if  result is None:
         raise HTTPException(
-            status_code=404, detail=f"Cell with cell_id=={cell_id} and campaign_id=={campaign_id} and surface_id=={surface_id} not found"
+            status_code=404, detail=f"Cell with id=={cell_id} not found"
         )
     return result
 
@@ -85,12 +112,27 @@ def delete_cell(   *,
     """
     Delete a cell in the database.
     """
+    hive = crud.hive.get(db=db, id=hive_id)
+    if hive is None:
+        raise HTTPException(status_code=404, detail=f"Hive with id=={hive_id} not found"  )
+    
+    Campaign = crud.campaign.get_campaign(db=db, campaign_id=campaign_id, hive_id=hive_id)
+    if Campaign is None:
+        raise HTTPException(
+            status_code=404, detail=f"Campaign with id=={campaign_id} not found"
+        )
+    surface = crud.surface.get_surface_by_ids(db=db, surface_id=surface_id,campaign_id=campaign_id)
+    if  surface is None:
+        raise HTTPException(
+            status_code=404, detail=f"Surface with IDs id=={surface_id}  not found"
+        )
+    
     result = crud.cell.get_Cell(db=db, cell_id=cell_id, surface_id=surface_id, campaign_id=campaign_id)
     if  result is None:
         raise HTTPException(
-            status_code=404, detail=f"Cell with cell_id=={cell_id} and campaign_id=={campaign_id} and surface_id=={surface_id} not found"
+            status_code=404, detail=f"Cell with id=={cell_id} not found"
         )
-    updated_recipe = crud.cell.remove(db=db, cell=result)
+    crud.cell.remove(db=db, cell=result)
     return  {"ok": True}
 
 @api_router_cell.post("/",status_code=201, response_model=Cell)
@@ -107,19 +149,26 @@ def create_cell(
     """
     Create a new cell in the surface_id of the campaign_id of the hive_id
     """
+    hive = crud.hive.get(db=db, id=hive_id)
+    if hive is None:
+        raise HTTPException(status_code=404, detail=f"Hive with id=={hive_id} not found"  )
+    
+    Campaign = crud.campaign.get_campaign(db=db, campaign_id=campaign_id, hive_id=hive_id)
+    if Campaign is None:
+        raise HTTPException(
+            status_code=404, detail=f"Campaign with id=={campaign_id} not found"
+        )
     surface = crud.surface.get_surface_by_ids(db=db, surface_id=surface_id,campaign_id=campaign_id)
     if  surface is None:
         raise HTTPException(
-            status_code=404, detail=f"Surface with surface_id=={surface_id} and campaign_id={campaign_id } not found"
+            status_code=404, detail=f"Surface with IDs id=={surface_id}  not found"
         )
-    # boundary=crud.boundary.get_Boundary_by_ids(db=db, surface_id=surface.id)
+    
     centro=surface.boundary.centre
     point=recipe_in.centre
     distancia= math.sqrt((centro['Longitude'] - point['Longitude'])**2+(centro['Latitude']-point['Latitude'])**2)
     if distancia<=surface.boundary.radius:
-    
         cell = crud.cell.create_cell(db=db, obj_in=recipe_in,surface_id=surface_id)
-        
         background_tasks.add_task(create_slots_cell, surface,hive_id,cell.id)
         return cell
     else:
@@ -128,10 +177,6 @@ def create_cell(
         )
    
 
-import asyncio
-from fastapi_utils.session import FastAPISessionMaker
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:mypasswd@localhost:3306/SocioBee"
-sessionmaker = FastAPISessionMaker(SQLALCHEMY_DATABASE_URL)
 async def create_slots_cell(surface: Surface,hive_id:int,cell_id:int):
     """
     Create all the slot of each cells of the campaign. 
@@ -142,13 +187,13 @@ async def create_slots_cell(surface: Surface,hive_id:int,cell_id:int):
         #       for cam in campaigns:
         # if cam.start_datetime.strftime("%m/%d/%Y, %H:%M:%S")==date_time:
         cam=crud.campaign.get_campaign(db=db,hive_id=hive_id,campaign_id=surface.campaign_id)
-        duration= cam.end_datetime - cam.start_datetime
-        n_slot = duration//cam.sampling_period
-        if duration % cam.sampling_period != 0:
-            n_slot = n_slot+1
+        duration= cam.end_datetime - cam.start_datetime 
+        n_slot = int(duration.total_seconds()//cam.sampling_period)
+        if duration.total_seconds() % cam.sampling_period != 0:
+                n_slot = n_slot+1
         for i in range(n_slot):
             time_extra=i*cam.sampling_period
-            start = cam.start_datetime + timedelta(seconds=time_extra)
+            start = cam.start_datetime + timedelta(seconds=time_extra-1)
             end = start + timedelta(seconds=cam.sampling_period)
             # for sur in cam.surfaces:
                 # for cells in sur.cells:
@@ -171,47 +216,68 @@ def update_cell(
     """
     Update Campaign with campaign_id 
     """
-    cell = crud.cell.get_Cell(db=db,cell_id=cell_id)
-    # .get_campaign(db=db,hive_id=hive_id,campaign_id=campaign_id)
-    if not cell:
+    hive = crud.hive.get(db=db, id=hive_id)
+    if hive is None:
+        raise HTTPException(status_code=404, detail=f"Hive with id=={hive_id} not found"  )
+    
+    Campaign = crud.campaign.get_campaign(db=db, campaign_id=campaign_id, hive_id=hive_id)
+    if Campaign is None:
         raise HTTPException(
-            status_code=400, detail=f"Recipe with hive_id=={hive_id} and campaign_id=={campaign_id} and surface_id={surface_id} not found."
+            status_code=404, detail=f"Campaign with id=={campaign_id} not found"
+        )
+    surface = crud.surface.get_surface_by_ids(db=db, surface_id=surface_id,campaign_id=campaign_id)
+    if  surface is None:
+        raise HTTPException(
+            status_code=404, detail=f"Surface with IDs id=={surface_id}  not found"
+        )
+
+    cell = crud.cell.get_Cell(db=db,cell_id=cell_id)
+    if  cell is None:
+        raise HTTPException(
+            status_code=400, detail=f"Cell with id=={cell_id} not found."
         )
     # if recipe.submitter_id != current_user.id:
     #     raise HTTPException(
     #         status_code=403, detail=f"You can only update your recipes."
     #     )
-
-    updated_recipe = crud.cell.update(db=db, db_obj=cell, obj_in=recipe_in)
-    db.commit()
-    return updated_recipe
-
-
-@api_router_cell.patch("/{cell_id}", status_code=201, response_model=Cell)
-def update_parcially_cell(
-    *,
-    recipe_in: Union[CellUpdate,Dict[str, Any]],
-    hive_id:int,
-    campaign_id:int,
-    surface_id:int,
-    cell_id:int,
-    db: Session = Depends(deps.get_db)
-) -> dict:
-    """
-     Partially Update Campaign with campaign_id 
-    """
-    cell = crud.cell.get_Cell(db=db,cell_id=cell_id)
-    # .get_campaign(db=db,hive_id=hive_id,campaign_id=campaign_id)
-    if not cell:
+    centro=surface.boundary.centre
+    point=recipe_in.centre
+    distancia= math.sqrt((centro['Longitude'] - point['Longitude'])**2+(centro['Latitude']-point['Latitude'])**2)
+    if distancia<=surface.boundary.radius:
+        updated_recipe = crud.cell.update(db=db, db_obj=cell, obj_in=recipe_in)
+        db.commit()
+        return updated_recipe
+    else:
         raise HTTPException(
-            status_code=400, detail=f"Recipe with hive_id=={hive_id} and campaign_id=={campaign_id} and surface_id={surface_id} not found."
+            status_code=400, detail=f"Invalid request."
         )
-    # if recipe.submitter_id != current_user.id:
-    #     raise HTTPException(
-    #         status_code=403, detail=f"You can only update your recipes."
-    #     )
 
-    updated_recipe = crud.cell.update(db=db, db_obj=cell, obj_in=recipe_in)
-    db.commit()
-    return updated_recipe
+
+# @api_router_cell.patch("/{cell_id}", status_code=201, response_model=Cell)
+# def update_parcially_cell(
+#     *,
+#     recipe_in: Union[CellUpdate,Dict[str, Any]],
+#     hive_id:int,
+#     campaign_id:int,
+#     surface_id:int,
+#     cell_id:int,
+#     db: Session = Depends(deps.get_db)
+# ) -> dict:
+#     """
+#      Partially Update Campaign with campaign_id 
+#     """
+#     cell = crud.cell.get_Cell(db=db,cell_id=cell_id)
+#     # .get_campaign(db=db,hive_id=hive_id,campaign_id=campaign_id)
+#     if not cell:
+#         raise HTTPException(
+#             status_code=400, detail=f"Recipe with hive_id=={hive_id} and campaign_id=={campaign_id} and surface_id={surface_id} not found."
+#         )
+#     # if recipe.submitter_id != current_user.id:
+#     #     raise HTTPException(
+#     #         status_code=403, detail=f"You can only update your recipes."
+#     #     )
+
+#     updated_recipe = crud.cell.update(db=db, db_obj=cell, obj_in=recipe_in)
+#     db.commit()
+#     return updated_recipe
 
