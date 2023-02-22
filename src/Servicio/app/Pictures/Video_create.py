24fe60f9fb73aaf3b8125b97290dc26e27925e6e
mpuerta004 <mpuerta004@hotmail.com>
2023-02-22 11:58:23 +0000

    update video_create

--- src/Servicio/app/Pictures/Video_create.py
+++ src/Servicio/app/Pictures/Video_create.py
@@ -3,18 +3,19 @@ import os
 
 img_array = []
 
-path = r"/home/ubuntu/carpeta_compartida_docker/RecommenderSystem/src/Servicio/app/Pictures/Recomendaciones"
+path = r"/home/ubuntu/carpeta_compartida_docker/RecommenderSystem/src/Servicio/app/Pictures/Measurements"
 archivos = sorted(os.listdir(path))
-
 for i in archivos: 
     dirArchivo = path + "/" + i
     print(i)
     img = cv2.imread(dirArchivo)
+    height, width  = img.shape[:2]
+
     cv2.putText(img, f"Recomendaciones con no popularidad en 3 o menos celdas", (80,180), cv2.FONT_HERSHEY_SIMPLEX , 1.5, (0,0,0))
     img_array.append(img)
     
 
-video = cv2.VideoWriter('CVC-08.avi', cv2.VideoWriter_fourcc(*'DIVX'), 4, ( 1500, 1500))
+video = cv2.VideoWriter('db0.wmv',cv2.VideoWriter_fourcc(*'mp4v'), 10, (width, height))
 
 
 for i in range(0, len(archivos)):
