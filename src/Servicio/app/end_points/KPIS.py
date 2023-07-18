71cc207dfa16070464f42f8698a2f5d075b08988
mpuerta004 <mpuerta004@hotmail.com>
2023-07-18 09:18:55 +0200

    Update KPIS.py

--- src/Servicio/app/end_points/KPIS.py
+++ src/Servicio/app/end_points/KPIS.py
@@ -36,7 +36,7 @@ from end_points.funtionalities import create_slots_campaign, create_cells_for_a_
 
 api_router_kpis = APIRouter(prefix="/kpis")
 
-@api_router_kpis.get("/hive/", status_code=200, response_model=float)
+@api_router_kpis.get("/hive/{hive_id}", status_code=200, response_model=float)
 def get_kpis_per_hive(    *,
     hive_id: int,
     db: Session = Depends(deps.get_db),
@@ -52,14 +52,14 @@ def get_kpis_per_hive(    *,
 
     #Step 2
     number_of_measurements= crud.measurement.get_number_of_measurements_per_hive(db=db,hive_id=hive_id)
-    print("Numero de recomendaciones realizadas", number_of_measurements)
+    print("Numero de mediciones realizadas", number_of_measurements)
     number_of_measurements=number_of_measurements[0][0]/13
 
     return number_of_measurements/number_of_recommendations
 
 
 
-@api_router_kpis.get("/campaign/", status_code=200, response_model=float)
+@api_router_kpis.get("/campaign/{campaign_id}", status_code=200, response_model=float)
 def get_kpis_per_campaign(    *,
     campaign_id: int,
     db: Session = Depends(deps.get_db),
@@ -69,13 +69,12 @@ def get_kpis_per_campaign(    *,
 
     #STEP 1
     list_number=crud.recommendation.get_number_of_recommendacions_per_campaign(db=db,campaign_id=campaign_id)
-  
     number_of_recommendations=len(list_number)
     print("Numero de recomendationes realizadas", number_of_recommendations)
     
     #Step 2
     number_of_measurements= crud.measurement.get_number_of_measurements_per_campaign(db=db,campaign_id=campaign_id)
-    print("Numero de recomendaciones realizadas", number_of_measurements)
+    print("Numero de mediciones realizadas", number_of_measurements)
     number_of_measurements=number_of_measurements[0][0]/13
     return number_of_measurements/number_of_recommendations
 
