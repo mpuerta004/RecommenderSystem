c42e5bdc01863641511ff7be673c90f84a32e5c1
mpuerta004 <mpuerta004@hotmail.com>
2023-10-25 14:51:09 +0000

    create video file

---
+++ src/Servicio/app/Pictures/video.py
@@ -0,0 +1,30 @@
+import os
+from html2image import Html2Image
+import moviepy.video.io.ImageSequenceClip
+
+input_folder =  'C:/Users/mpuer/Desktop/recommendersystem/src/Servicio/app/Pictures/Recomendaciones_html_others'
+
+output_folder = 'C:/Users/mpuer/Desktop/RecommenderSystem/src/Servicio/app/Pictures/Recomendaciones_html_others_pictures'
+
+hti = Html2Image(output_path=output_folder)
+
+html_files = sorted(os.listdir(input_folder))
+
+for html_file in html_files:
+    if html_file.endswith(".html"):
+        hti.screenshot(url=input_folder+'/'+html_file, save_as=html_file+'.png')
+
+############# CRªEATE VIDEO #################
+fps=1
+#more fps means faster video speed
+# if we want a video for a spoken presentation (In the background) -> 1 fps
+# if we want a video for a focus presentation on this -> 2 or 3 fps
+   
+#List of pictures of the folder.
+image_files = [os.path.join(output_folder,img)
+               for img in os.listdir(output_folder)
+               if img.endswith(".png")]
+#create the video. w
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+clip.write_videofile('C:/Users/mpuer/Desktop/RecommenderSystem/src/Servicio/app/Pictures/bio-inspired_measurements.mp4')
+
