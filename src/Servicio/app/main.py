83f765113d9c4bad9e57b8c95e088a7e4453627c
Maite Puerta Beldarrain <mpuerta004@deusto.es>
2024-03-08 20:09:11 +0800

    update

--- src/Servicio/app/main.py
+++ src/Servicio/app/main.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
 
     # Add this line to run the system.
     scheduler = BackgroundScheduler()
-    scheduler.add_job(final_funtion, 'interval', seconds=60)
+    scheduler.add_job(final_funtion, 'interval', seconds=120)
     scheduler.add_job(State_change, 'interval', seconds=420)
     scheduler.start()
 
